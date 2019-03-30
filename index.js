f1fcd588ea3a6057c7abc35e06a9c840903e98c1
Mickey <micksheridan.24@gmail.com>
2019-03-30 10:33:03 -0400

    submit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+  return Math.abs(street - 42);
+}
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, finish) {
+  return Math.abs(start * 264 - finish * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
+  let price = 0;
+  if (distance > 2500) {
+    return "cannot travel that far";
+  } else if (distance > 2000) {
+    return 25;
+  } else {
+    distance -= 400;
+    price = distance * 0.02;
+  }
+  return price > 0 ? price : 0;
+}
